f677d80066bc2d8a6f2aef74f273c870b389d235
Djulianofre <87614263+Djulianofre@users.noreply.github.com>
2025-04-12 10:59:00 -0300

    Delete app directory

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>PlayRifa OAuth Redirect</h1>
-      <p>Este serviço redireciona códigos de autorização OAuth para o aplicativo PlayRifa.</p>
-      <p>Acesse <code>/api/oauth?code=seu_codigo</code> para testar o redirecionamento.</p>
-    </main>
-  );
-}
