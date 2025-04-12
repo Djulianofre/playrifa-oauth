c83a18b3c577704612f3adbabe2c7b6a74f4a9e5
Djulianofre <87614263+Djulianofre@users.noreply.github.com>
2025-04-12 11:39:52 -0300

    Delete app/(site)/page.tsx

--- app/(site)/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>PlayRifa OAuth Redirect</h1>
-      <p>Acesse /api/oauth?code=seu_codigo para testar o redirecionamento.</p>
-    </main>
-  );
-}
