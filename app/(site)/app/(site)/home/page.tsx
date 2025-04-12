b6404465243b42c3619a950480a7ab4abad1c0c9
Djulianofre <87614263+Djulianofre@users.noreply.github.com>
2025-04-12 11:44:03 -0300

    Delete app/(site)/app/(site)/home directory

--- app/(site)/app/(site)/home/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/(site)/page.tsx
-export default function Home() {
-  return (
-    <main>
-      <h1>Redirecionamento OAuth do PlayRifa</h1>
-      <p>Acesse <code>/api/oauth?code=seu_codigo</code> para testar o redirecionamento.</p>
-    </main>
-  );
-}
