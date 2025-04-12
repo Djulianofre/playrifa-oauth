ca1e1561b476484a56e5e8894d805b203ed796fe
Djulianofre <87614263+Djulianofre@users.noreply.github.com>
2025-04-12 11:25:28 -0300

    Create page.tsx

---
+++ app/(site)/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>PlayRifa OAuth Redirect</h1>
+      <p>Acesse /api/oauth?code=seu_codigo para testar o redirecionamento.</p>
+    </main>
+  );
+}
